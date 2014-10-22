bb2840f9b629e8c7d96755401492e8aea723f5d0
fergaldoyle <fergal_doyle@yahoo.com>
2014-10-22 22:51:49 +0100

    Tests updated

--- test/main.js
+++ test/main.js
@@ -6,17 +6,12 @@ var assert = require('stream-assert');
 
 require('mocha');
 
-console.log('Starting directory: ' + process.cwd());
 process.chdir('./test');
-console.log('New directory: ' + process.cwd());
-
-var fixtures = function (glob, dir) {
-	return path.join(__dirname, 'fixtures' + (dir ? dir : 'A'), glob);
-}
+console.log('cwd: ' + process.cwd());
 
 describe('gulp-require-angular', function () {
 	describe('requireAngular()', function () {
-
+		/***/
 		it('should throw, when arguments are missing', function () {
 			(function () {
 				requireAngular();
@@ -24,7 +19,7 @@ describe('gulp-require-angular', function () {
 		});
 
 		it('should emit error on streamed file', function (done) {
-			gulp.src(fixtures('**\/*.js'), { buffer: false })
+			gulp.src('.\/fixturesA/**\/*.js', { buffer: false })
 				.pipe(requireAngular('myApp'))
 				.on('error', function (err) {
 					err.message.should.eql('Streaming not supported');
@@ -33,7 +28,7 @@ describe('gulp-require-angular', function () {
 		});
 
 		it('should emit error when bad mainModule is passed', function (done) {
-			gulp.src(fixtures('**\/*.js'))
+			gulp.src('.\/fixturesA/**\/*.js')
 				.pipe(requireAngular('foob'))
 				.on('error', function (err) {
 					err.message.should.eql('Did not find any file where angular module \'foob\' is defined');
@@ -42,7 +37,7 @@ describe('gulp-require-angular', function () {
 		});
 
 		it('should emit error if module dependency is not found', function (done) {
-			gulp.src(fixtures('**\/*.js', 'C'))
+			gulp.src('.\/fixtureC/**\/*.js')
 				.pipe(requireAngular('myApp', { errorOnMissingModules: true }))
 				.on('error', function (err) {
 					done();
@@ -50,7 +45,7 @@ describe('gulp-require-angular', function () {
 		});
 
 		it('should emit end', function (done) {
-			gulp.src(fixtures('**\/*.js'))
+			gulp.src('.\/fixturesA/**\/*.js')
 				.pipe(requireAngular('myApp'))
 				.on('end', function () {
 					done();
@@ -58,14 +53,14 @@ describe('gulp-require-angular', function () {
 		});
 
 		it('should emit a single file', function (done) {
-			gulp.src(fixtures('**\/*.js'))
+			gulp.src('.\/fixturesA/**\/*.js')
 				.pipe(requireAngular('myApp'))
 				.pipe(assert.length(1))
 				.pipe(assert.end(done));
 		});
 
 		it('should include required modules (only)', function (done) {
-			gulp.src(fixtures('**\/*.js'))
+			gulp.src('.\/fixturesA/**\/*.js')
 				.pipe(requireAngular('myApp'))
 				.pipe(assert.first(function (f) {
 					f.contents.toString().should
@@ -79,48 +74,34 @@ describe('gulp-require-angular', function () {
 		});
 
 		it('should include files where required modules (only) are defined and referenced', function (done) {
-			gulp.src(fixtures('**\/*.js'))
+			gulp.src('.\/fixturesA/**\/*.js')
 				.pipe(requireAngular('myApp', { relativeTo: './fixturesA' }))
 				.pipe(assert.first(function (f) {
 					f.contents.toString().should
-					.containEql('./app.js')
-					.and.containEql('./moduleA/moduleA.js')
-					.and.containEql('./moduleB/moduleB.js')
-					.and.containEql('./moduleD/moduleD.js')
-					.and.containEql('./app/app.controller.js')
-					.and.containEql('./moduleA/moduleA.animation.js')
-					.and.containEql('./moduleA/moduleA.config.js')
-					.and.containEql('./moduleA/moduleA.constant.js')
-					.and.containEql('./moduleA/moduleA.controller.js')
-					.and.containEql('./moduleA/moduleA.directive.js')
-					.and.containEql('./moduleA/moduleA.factory.js')
-					.and.containEql('./moduleA/moduleA.filter.js')
-					.and.containEql('./moduleA/moduleA.provider.js')
-					.and.containEql('./moduleA/moduleA.run.js')
-					.and.containEql('./moduleA/moduleA.service.js')
-					.and.containEql('./moduleA/moduleA.value.js')
-					.and.not.containEql('./moduleC/moduleC.js')
-					.and.not.containEql('./moduleC/moduleC.controller.js')
+					.containEql("'./app.js'")
+					.and.containEql("'./moduleA/moduleA.js'")
+					.and.containEql("'./moduleB/moduleB.js'")
+					.and.containEql("'./moduleD/moduleD.js'")
+					.and.containEql("'./app/app.controller.js'")
+					.and.containEql("'./moduleA/moduleA.animation.js'")
+					.and.containEql("'./moduleA/moduleA.config.js'")
+					.and.containEql("'./moduleA/moduleA.constant.js'")
+					.and.containEql("'./moduleA/moduleA.controller.js'")
+					.and.containEql("'./moduleA/moduleA.directive.js'")
+					.and.containEql("'./moduleA/moduleA.factory.js'")
+					.and.containEql("'./moduleA/moduleA.filter.js'")
+					.and.containEql("'./moduleA/moduleA.provider.js'")
+					.and.containEql("'./moduleA/moduleA.run.js'")
+					.and.containEql("'./moduleA/moduleA.service.js'")
+					.and.containEql("'./moduleA/moduleA.value.js'")
+					.and.not.containEql("'./moduleC/moduleC.js'")
+					.and.not.containEql("'./moduleC/moduleC.controller.js'")
 				}))
 				.pipe(assert.end(done));
 		});
-		/*
-		it('should find modules in bower.json', function (done) {
-			process.chdir('./test')
-			gulp.src(fixtures('**\/*.js'))
-				.pipe(requireAngular('myApp'))
-				.pipe(assert.first(function (f) {
-					//f.contents.toString().should
-					//.containEql('./foopath/app.js')
-					//.and.containEql('./foopath/moduleA/moduleA.config.js')
-				}))
-				.pipe(assert.end(done));
 
-			done();
-		});
-		*/
 		it('should order the files correctly', function (done) {
-			gulp.src(fixtures('**\/*.js'))
+			gulp.src('.\/fixturesA/**\/*.js')
 				.pipe(requireAngular('myApp', { relativeTo: './fixturesA' }))
 				.pipe(assert.first(function (f) {
 					var lines = f.contents.toString().split('\n');
@@ -131,19 +112,34 @@ describe('gulp-require-angular', function () {
 		});
 
 		it('should include third party modules', function (done) {
-			gulp.src(fixtures('**\/*.js', 'B'))
+			gulp.src('.\/fixturesB/**\/*.js')
 				.pipe(requireAngular('thirdParty', { relativeTo: './fixturesB' }))
 				.pipe(assert.first(function (f) {
 					f.contents.toString().should
-						.containEql('./app.js')
-						.and.containEql('./ui/angular-ui-router.js')
-						.and.containEql('./ui/keypress.js')
-						.and.containEql('./ng/angular-route.js')
-						.and.containEql('./ng/angular-sanitize.js')
+						.containEql("'./app.js'")
+						.and.containEql("'./ui/angular-ui-router.js'")
+						.and.containEql("'./ui/keypress.js'")
+						.and.containEql("'./ng/angular-route.js'")
+						.and.containEql("'./ng/angular-sanitize.js'")
 				}))
 				//.pipe(gulp.dest('./'))
 				.pipe(assert.end(done));
 		});
+
+		it('should find modules in bower.json', function (done) {
+			gulp.src('.\/fixturesD/**\/*.js')
+				.pipe(requireAngular('myApp', { bower: true, relativeTo: './fixturesD' }))
+				.pipe(assert.first(function (f) {
+					f.contents.toString().should
+					.containEql("'./app.js'")
+					.and.containEql("'./../bower_components/moduleA/release/moduleA.js'")
+					.and.containEql("'./../bower_components/moduleB/release/moduleB.js'")
+				}))
+				.pipe(gulp.dest('../'))
+				.pipe(assert.end(done));
+		});
+
+		/***/
 
 	});
 });
